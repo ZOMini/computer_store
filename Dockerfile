39509c0126f7eb918f70aacc3174157c597f7b91
ZOMini <zarikacr@gmail.com>
2022-07-23 00:52:35 +0300

    v 2.3

--- Dockerfile
+++ Dockerfile
@@ -3,4 +3,4 @@ FROM python:3.9.7-slim
 WORKDIR /c_store
 COPY c_store/ .
 RUN pip install -r requirements.txt
-CMD ["gunicorn", "c_store.wsgi:application", "--bind", "0:8000" ]
+CMD ["gunicorn", "c_store.wsgi:application", "--bind", "127.0.0.1:8000" ]
--- c_store/c_store/settings.py
+++ c_store/c_store/settings.py
@@ -6,7 +6,7 @@ SECRET_KEY = 'zpj#r!xryf0o75rlt9n!k(@ijotd#!&ozt1n+zflols!p%-*%&'
 
 DEBUG = True
 
-ALLOWED_HOSTS = ['*']
+ALLOWED_HOSTS = ['web', 'web:8000','127.0.0.1','51.250.74.84','*']
 
 INTERNAL_IPS = ['127.0.0.1',]
 
@@ -72,7 +72,7 @@ DATABASES = {
            'NAME': 'postgres',
            'USER': 'postgres',
            'PASSWORD': 'postgres',
-           'HOST': '127.0.0.1',
+           'HOST': 'db',
            'PORT': '5432',
         }
     }
